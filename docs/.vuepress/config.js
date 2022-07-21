64eee94e5665e5e93b1c78ba92496df495976a28
二丫讲梵 <eryajf@163.com>
2022-07-21 11:25:58 +0800

    首次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
+module.exports = {
+  theme: 'vdoing', // 使用npm包主题
+  title: "vdoing-template",
+  description: 'vdoing博客主题模板',
+  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+  head,
+  plugins,
+  themeConfig,
+
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "adb9fb0ac1159e00ce7f",
+        clientSecret: "27da8dc85f808c2bd1b6e44da5ae69c4ddf17d8d",
+        owner: "eryajf",
+        repo: "vdoing-template",
+      },
+    ],
+  ],
+}
---
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -0,0 +1,9 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  { text: 'one',link: '/one/',},
+  { text: 'two',link: '/two/',},
+  { text: 'three', link: '/gthree/' },
+  { text: '留言板', link: '/message-board/' },
+  { text: '我的博客', link: 'https://wiki.eryajf.net' },
+]
