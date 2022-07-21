522e2acf3b3d90f246ee93ddfd952bdbf4c6a0d0
二丫讲梵 <eryajf@163.com>
2022-07-21 11:32:01 +0800

    🥳 添加图片

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -3,7 +3,7 @@ module.exports = [
   { text: '首页', link: '/' },
   { text: 'one',link: '/one/',},
   { text: 'two',link: '/two/',},
-  { text: 'three', link: '/gthree/' },
+  { text: 'three', link: '/three/' },
   { text: '留言板', link: '/message-board/' },
   { text: '我的博客', link: 'https://wiki.eryajf.net' },
 ]
